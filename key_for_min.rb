9da282ba4cf1807f5781347a765fb122bec72193
Yelstin Fernandes <yfernan2@u.rochester.edu>
2017-05-06 18:24:04 +0000

    Learn IDE wasn't recognizing my pull request

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,8 @@
 def key_for_min_value(name_hash)
+
   return nil if name_hash == {}
   min = name_hash.collect {|k,v| v}.first
   name_hash.each {|k,v| min = v if v < min}
   name_hash.each {|k,v| return k if v == min}
+  
 end
