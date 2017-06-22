b5a5150236ddd254dbdbdc57dcd9def67dd0316a
Kesean Woodhouse <kesean.woodhouse@gmail.com>
2017-06-22 13:55:21 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,15 @@
 
 def key_for_min_value(name_hash)
 
+  return nil if name_hash.empty?
+  lowest_key = nil
+  name_hash.each do |key,value|
+    if lowest_key == nil
+      lowest_key = key
+    elsif name_hash[lowest_key] > name_hash[key]
+      lowest_key = key
+    end
+  end
+  return lowest_key
+
 end
