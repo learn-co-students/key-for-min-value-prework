5e9ad2a3679a6144f494f5f0e6df3e1cec045735
Justin Manalad <jstn.mnld@gmail.com>
2017-07-07 20:16:26 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,19 @@
 
 def key_for_min_value(name_hash)
 
+current_minimum_value = 0
+current_minimum_key = nil
+
+  name_hash.each do |key, value|
+
+    if value < current_minimum_value || current_minimum_value == 0
+      current_minimum_value = value
+      current_minimum_key = key
+
+    end
+
+  end
+
+    current_minimum_key
+
 end
