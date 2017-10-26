a556d8a47aaae45cd7265e891c9d5d6aedc2e296
Nemanja Drakulovic <ndrakulovic@hotmail.com>
2017-10-26 03:28:00 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,29 +2,39 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  if name_hash == {}
-    return nil
-  end
+  # if name_hash == {}
+  #   return nil
+  # end
+  #
+  # name_array = []
+  # age_array = []
+  # name_hash.each do |name, age|
+  #   name_array.push(name)
+  #   age_array.push(age)
+  # end
+  #
+  # minimum = age_array[0]
+  # age_array.each do |age|
+  #   if minimum > age
+  #     minimum = age
+  #   end
+  # end
+  #
+  # name_array.each do |name|
+  #   if name_hash[name] == minimum
+  #     return name
+  #   end
+  # end
 
-  name_array = []
-  age_array = []
-  name_hash.each do |name, age|
-    name_array.push(name)
-    age_array.push(age)
-  end
-
-  minimum = age_array[0]
-  age_array.each do |age|
-    if minimum > age
-      minimum = age
+  low_key = nil
+  low_value = nil
+  name_hash.each do |key,value|
+    if low_value == nil || low_value > value
+      low_value = value
+      low_key = key
     end
   end
-
-  name_array.each do |name|
-    if name_hash[name] == minimum
-      return name
-    end
-  end
+  low_key
 end
 
 name_hash = {adam:15,blake:10,amanda:12,robert:9}
