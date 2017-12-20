a6b32767115d3eb5406bb108086bf039ecf24266
Christopher Diep <diep.christopher@gmail.com>
2017-12-20 19:22:01 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,8 +9,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |key,value|
     if min_value == nil
       min_value = value
-    end
-    if min_value > value
+    elsif min_value > value
       min_value = value
     end
   end
