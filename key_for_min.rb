f4f12cdd4a1ab1932f7379626ee621ad6028f767
Dwire <gjdwyer8@gmail.com>
2017-12-19 23:47:50 -0500

    Pass all key-for-min tests

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,27 @@
 
 def key_for_min_value(name_hash)
 
+  ages = name_hash.collect do |name, age|
+    age
+  end
+
+  counter = 0
+  ages.each do |num|
+  if counter == 0
+    counter = num
+  elsif num < counter
+      counter = num
+    end
+  end
+
+return_sybol = nil
+  name_hash.each do |name, age|
+    if name_hash[name] == counter
+      return_sybol = name
+    end
+  end
+
+return_sybol
+
+
 end
