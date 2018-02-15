86c07daaac114806ed23709dca72f8e7c939783f
Harim Choi <harim1206@gmail.com>
2018-02-15 15:44:45 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,23 @@
 
 def key_for_min_value(name_hash)
 
+  x = []
+  puts("name_hash: #{name_hash}")
+
+  name_hash.map{ |name, key|
+
+    x.push(key)
+
+  }
+
+  name_hash.each{ |name, key|
+
+    if key == x.min
+      return name
+    end
+  }
+
+
+  puts("x: #{x}")
+
 end
