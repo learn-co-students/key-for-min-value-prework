72e9cefd25d0d167b5108a28b4d1a49a3633ec3b
briecodes <hello@brie.work>
2018-03-17 22:09:13 -0400

    Min Value / Key Method Created.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,25 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  if name_hash.empty?
+    return nil
+  else
+    array = []
+    number = 0;
+    i = 0
+    name_hash.collect do |key, value|
+      if number == 0
+        number = value
+      else
+        if number > value
+          number = value
+        end
+      end
+    end
+    name_hash.each do |key, value|
+      if value == number
+        return key
+      end
+    end
+  end
 end
