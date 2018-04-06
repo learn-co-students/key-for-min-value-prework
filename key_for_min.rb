afe25713928bfce1d0db22f2e445fbaadc314501
travisdock@gmail.com <travisdock@gmail.com>
2018-04-06 15:09:07 -0400

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,21 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  if name_hash.empty?
+    return nil
+  else
+    a = 100**100
+    name_hash.collect do |key, value|
+      if value < a
+        a = value
+      end
+    end
+    name_hash.invert[a]
+
+    #Other solution that also passed all tests but used .sort_by
+    #name_hash = name_hash.sort_by {|key, value| value}
+    #name_hash[0][0]
+  end
+
 
 end
