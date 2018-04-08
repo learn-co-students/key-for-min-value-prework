dc5e963d59c70daefdff65724e910061d22fe393
github username <github email address>
2018-04-08 14:09:02 -0700

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,27 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  current = nil
+
+  if name_hash == {}
+    return nil
+  end
+
+  name_hash.each do |key, value| 
+    current = value
+    break
+  end
+
+  name_hash.each do |key, value|
+    if value < current
+      current = value
+    end
+  end
+
+  name_hash.each do |key, value|
+    if value == current
+      return key
+    end
+  end
 
 end
