52dcc641c41f2b67cf918dc9093b253f73bae7d6
jarretbryan <jarret.bryan@gmail.com>
2018-04-15 17:36:08 -0400

    Need to figure out sort

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,9 +4,14 @@
 def key_for_min_value(name_hash)
   minimum_value = nil
   minimum_key = nil
-  name_hash.each do |key, value|
-    if minimum_value = nil
-      return key
+  #keys and values would be nil were the hash empty - the above variables would account for this
+  #we can mutate these variables later if it's not the case
+  #below we're going to iterate
+  name_hash.each do |key,value|
+    if minimum_value == nil
+    #so, our minimum value is nil OR.....?  
+      minimum_value = value
+      minimum_key = key
     end
   end
   minimum_key
