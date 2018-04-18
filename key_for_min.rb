57ee1548269a5b0267f61a9c8c7c68324959ad48
jarretbryan <jarret.bryan@gmail.com>
2018-04-17 22:03:23 -0400

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,14 +4,14 @@
 def key_for_min_value(name_hash)
   minimum_value = nil
   minimum_key = nil
-  test_value = name_hash.each{|key, value| value}
   #keys and values would be nil were the hash empty - the above variables would account for this
   #we can mutate these variables later if it's not the case
   #below we're going to iterate
   name_hash.each do |key, value|
-    if minimum_value == nil || value < test_value
+    if minimum_value == nil || value < minimum_value
       #test_value above doesn't working. can't compare integer to ?? why?
       #why am i using minimum_value? wont that always be true?
+      minimum_value = value
       minimum_key = key
     end
   end
