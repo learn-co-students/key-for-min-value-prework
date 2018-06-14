83240cdb8aa99b1a4be8448f4a47ee4db3cbe275
github username <github email address>
2018-06-14 15:41:43 -0700

    key for min commit

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  ki = []
+  val = []
+  name_hash.collect do |x, y|
+    ki.push(x)
+    val.push(y)
+  end
+  if name_hash.empty?
+    nil
+  else
+    ki[val.index(val.min)]
+  end
 
 end
