06b38f63eaa4aae95b677c367dadb8e76416ce0a
Maduri Vassaramo <maduri.vassaramo@hotmail.co.uk>
2018-06-24 19:18:57 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+ if name_hash.empty? 
+   return nil 
+   
+else 
 
+smallest_key = ""
+name_hash_sorted = {}
+name_hash_sorted = name_hash.sort_by  { |key, value| value } 
+
+smallest_key = name_hash_sorted[0]
+return smallest_key.first 
+end
 end
