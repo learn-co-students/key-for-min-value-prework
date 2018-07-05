ffe2f6c129bb71ba3bb24cb957c739510a5db58f
ritz-w <ritz.wu@outlook.com>
2018-07-05 12:45:46 +0100

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,21 +4,25 @@
 def key_for_min_value(name_hash)
   counter = 0
   value_sort = []
-  name_hash.each do |key, value|
-    value_sort<< value
-  end
-  min = value_sort[counter]
-  value_sort.each do |value|
-    min = value_sort[0]
-    if min >= value_sort[counter]
-      min = value_sort[counter]
-      counter += 1
+  if name_hash.size > 0
+    name_hash.each do |key, value|
+      value_sort<< value
     end
-  end
-  name_hash.each do |key, value|
-    if value === min
-    print key
-    return key
+    min = value_sort[counter]
+    value_sort.each do |value|
+      min = value_sort[0]
+      if min >= value_sort[counter]
+        min = value_sort[counter]
+        counter += 1
+      end
     end
+    name_hash.each do |key, value|
+      if value === min
+      print key
+      return key
+      end
+    end
+  else
+    return nil
   end
 end
