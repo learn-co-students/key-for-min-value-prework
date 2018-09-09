a64f37f4bf79aa9cf251df054ee4bfa9678b383a
N-S-practice-code <nico.ehs05@gmail.com>
2018-09-09 18:51:31 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  r=nil
+  min=nil
+  name_hash.each do |k, v|
+    if min==nil or v<min
+      r=k
+      min=v
+    end
+  end
+  r
 end
