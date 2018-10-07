756a3cc2043290ded4a89829a605b80c369178ae
Hannah C <hannahchang17@gmail.com>
2018-10-07 05:35:50 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,12 +3,12 @@
 
 def key_for_min_value(name_hash)
   small_number = nil 
-  return_item = nil 
+  return_this_item = nil 
   name_hash.collect do |item, number|
     if small_number == nil || number < small_number
       small_number = number
-      return_item = item
+      return_this_item = item
     end 
   end 
-  return_item
+  return_this_item
 end
