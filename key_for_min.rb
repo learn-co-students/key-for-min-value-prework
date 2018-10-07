a6ba5382a2c5ccb9493bc04583afa7eb937a5105
ScottDenton <scottyd_14@hotmail.com>
2018-10-07 11:16:19 -0700

    Corrected the [0] for [i]

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,15 +4,17 @@
 def key_for_min_value(name_hash)
   value_array = name_hash.collect {|key, value| value}
   key_array = name_hash.collect {|key, value| key}
-  first_value = value_array[0]
-  first_key = key_array[0]
+  i = 0
+  first_value = value_array[i]
+  first_key = key_array[i]
 
   name_hash.each do |key, value|
     if value < first_value
       first_value = value
       first_key = key
+    else i +=1 
     end
   end
-  first_key 
+  first_key
 
 end
