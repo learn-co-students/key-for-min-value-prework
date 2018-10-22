19a1c42e5f1b8cf0d479f9fd4f715722d6d234a0
heather sterman <heather.sterman531@gmail.com>
2018-10-22 17:34:32 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+lunch = {:salad => 5, :drink => 1, :cookie => 2}
+key_for_min_value(lunch)
 end
+
+lunch 
