10a78d7b116d3871858a366fe1806cebaedccac2
Connor Callahan <connorwcallahan@gmail.com>
2018-10-22 14:06:10 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,10 +4,10 @@
 def key_for_min_value(name_hash)
   min_key = nil
   min_value = nil 
-  name_hash.each do |hash_key, hash_value|
-    if min_value == nil || hash_value < min_value
-      min_value = hash_value
-      min_key = hash_key
+  name_hash.each do |key, value|
+    if min_value == nil || value < min_value
+      min_value = value
+      min_key = key
     end
   end
   return min_key
