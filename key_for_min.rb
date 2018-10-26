f85860182f0aa481df05642a4fd5f0512b083684
IMH51 <ianmartinharrison@outlook.com>
2018-10-26 17:10:26 +0100

    all tests pass without using banned methods

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,21 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    if name_hash == {}
+      return nil
+    end
+    values = name_hash.collect do |key,value|
+      value
+    end
+    low_value = values[0]
+    values.each do |x|
+      if x < low_value
+        low_value = x
+      end
+    end
+    name_hash.collect do |key,value|
+      if value == low_value
+        return key
+      end
+    end
 end
