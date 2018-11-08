13c3b1eda7af584cf6ad3676573a845bc86dd614
sensible-proc-7557 <vchan313@gmail.com>
2018-11-08 01:19:50 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,19 @@
 
 def key_for_min_value(name_hash)
 
+
+  winner_1 = 100
+  winner_2 = ''
+name_hash.each do |key, value|
+  if value < winner_1
+    winner_1 = value
+    winner_2 = key
+  end
+end
+
+if name_hash.length == 0
+  nil
+else
+winner_2
+end
 end
