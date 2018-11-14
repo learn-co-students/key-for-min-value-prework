951c782e8239b249cc3541e89c18441151479ebf
tfuquirk <timothyfquirk@gmail.com>
2018-11-13 21:43:01 -0500

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,21 @@
 
 def key_for_min_value(name_hash)
 
+  placeholder_key = nil
+  placeholder_value = nil
+
+  name_hash.each do |key, value|
+
+    if placeholder_key == nil
+      placeholder_key = key
+      placeholder_value = value
+    end
+
+    if value < placeholder_value
+      placeholder_key = key
+      placeholder_value = value
+    end
+
+  end
+  placeholder_key
 end
