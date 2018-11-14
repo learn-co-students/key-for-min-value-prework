b937a4c7d89c2b6c7f277025cf53684cff453abe
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-11-14 04:18:04 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,16 +7,17 @@ def key_for_min_value(name_hash)
     name_hash.collect do |key,value|
       array << value
     end
-    for j in 0..array.length-1
-      for i in 0..array.length-2-j 
+    for j in 0...array.length
+      for i in 0...array.length-1-j 
         if array[i] > array[i+1]
           array[i],array[i+1]=array[i+1],array[i]
         end
       end
     end
     name_hash.each do |key,value|
-      if value = array[0]
+      if value == array[0]
         return key
       end
     end
   end
+end
