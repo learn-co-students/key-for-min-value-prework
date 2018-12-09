332cc63095635fd4a5ca7974979e680d8d2c7190
cannoli9917 <katie.pennachio@gmail.com>
2018-12-09 10:11:49 -0500

    Key for min value solved

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,20 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    smallest_value = 0
+    smallest_key = ""
+    if name_hash == {}
+        return nil
+    end
+    name_hash.each_with_index do |(key, value), index|
+        if index == 0
+            smallest_value = value
+            smallest_key = key
+        elsif value < smallest_value
+            smallest_value = value
+            smallest_key = key
+        end
+    end
+    return smallest_key
 end
+
+
