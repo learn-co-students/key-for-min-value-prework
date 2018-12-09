380d10671433d75f5fdaeb0cd27288adb2f62d44
generous-program-0200 <harrycopley@vultuslimited.com>
2018-12-09 20:31:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  x_key = nil 
+  x_value = 0
+name_hash.each do |key, value|
+  if x_value == 0 || value < x_value
+    x_value = value
+    x_key = key
+   end
+  end
+x_key
 end
