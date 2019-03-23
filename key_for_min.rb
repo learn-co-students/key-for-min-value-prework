68c44a424c983c869741110acdbae5dc5c4f2745
Jose Elera <jelera@gmail.com>
2019-03-23 03:57:31 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,22 @@
 
 def key_for_min_value(name_hash)
 
+  if name_hash.empty?
+    nil
+  else
+
+    smallest_value = 999999999
+    smallest_key = ""
+
+    name_hash.each do |name, value|
+        if value < smallest_value
+          smallest_value = value
+          smallest_key = name
+        end
+    end
+
+    smallest_key
+
+  end
+
 end
