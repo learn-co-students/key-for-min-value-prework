022f613d89769c9510b8f269b0895e5ade0ec861
Ahmed Gaber <21stlimitless@gmail.com>
2019-06-01 19:02:17 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   lowest_value_name = nil
-  lowest_value = 1000000
+  lowest_value = 9999999
   name_hash.each do |name, value|
     if value < lowest_value
       lowest_value = value
