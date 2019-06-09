026ee93802c3c36905b2121b3ed5e983393aa42d
Varvara Babkova <varvara.munday@gmail.com>
2019-06-09 13:28:32 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  return nil if name_hash == {}
+  min_price = 1000
+  min_name = ""
 
+  name_hash.collect do |name, price|
+    if price < min_price
+      min_price = price
+      min_name = name
+    end
+  end
+  return min_name
 end
