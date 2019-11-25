c95b1e092ff7217897d9a7a1abd683fc36633819
dagonyclark <sean@dagony-clark.com>
2019-11-25 17:59:33 -0500

    Revert "Done."
    This reverts commit 9bb1ab5382676aa58f3375ff0bcfd0d8a1bd93bd.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,20 +2,5 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  keys_name = nil
-  # values_array = []
-  lowest_value = nil
-  # counter = 0
-  # name_hash.each do |key, value|
-  #   keys name << key
-  #   values_array << value
-  #   lowest_value[0] = value
-  # end
-  name_hash.each do |key, value|
-    if lowest_value == nil || lowest_value > value
-      lowest_value = value
-      keys_name = key
-    end
-  end
-  keys_name
+
 end
